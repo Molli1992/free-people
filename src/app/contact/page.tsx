c6dbafb5f7b47fac09492c76c92c8a7b5cc5dfa9
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-10-03 12:27:21 -0300

    first commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import Contact from '@/components/contact/contact';
+
+export default function ContactPage() {
+  return (
+    <div className="w-full h-screen">
+      <Contact />
+    </div>
+  );
+}
